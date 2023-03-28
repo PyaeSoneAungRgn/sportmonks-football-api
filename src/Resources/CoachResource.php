e8fc63d3cbdf6a04a63a699e3ed24fe7561a9571
igorlealantunes <igorlealantunes@gmail.com>
2023-03-28 19:28:45 -0300

    Update CoachResource.php

--- src/Resources/CoachResource.php
+++ src/Resources/CoachResource.php
@@ -4,35 +4,35 @@ namespace PyaeSoneAung\SportmonksFootballApi\Resources;
 
 class CoachResource extends BaseResource
 {
-    public function all(): array
+    public function all(): \stdClass
     {
         return $this->get(
             url: 'coaches'
         );
     }
 
-    public function byId(int|string $id): array
+    public function byId(int|string $id): \stdClass
     {
         return $this->get(
             url: "coaches/{$id}"
         );
     }
 
-    public function byCountryId(int|string $id): array
+    public function byCountryId(int|string $id): \stdClass
     {
         return $this->get(
             url: "coaches/countries/{$id}"
         );
     }
 
-    public function searchByName(string $search): array
+    public function searchByName(string $search): \stdClass
     {
         return $this->get(
             url: "coaches/search/{$search}"
         );
     }
 
-    public function lastUpdated(): array
+    public function lastUpdated(): \stdClass
     {
         return $this->get(
             url: 'coaches/latest'
