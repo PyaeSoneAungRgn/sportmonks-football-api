76e53f442740d6d16a7aaab1349609bcd8ace4b9
igorlealantunes <igorlealantunes@gmail.com>
2023-03-28 19:30:44 -0300

    Update InplayOddResource.php

--- src/Resources/InplayOddResource.php
+++ src/Resources/InplayOddResource.php
@@ -11,28 +11,28 @@ class InplayOddResource extends BaseResource
         );
     }
 
-    public function byFixtureId(int|string $id): array
+    public function byFixtureId(int|string $id): \stdClass
     {
         return $this->get(
             url: "odds/inplay/fixtures/{$id}"
         );
     }
 
-    public function byFixtureIdAndBookmakerId(int|string $fixtureId, int|string $bookmakerId): array
+    public function byFixtureIdAndBookmakerId(int|string $fixtureId, int|string $bookmakerId): \stdClass
     {
         return $this->get(
             url: "odds/inplay/fixtures/{$fixtureId}/bookmakers/{$bookmakerId}"
         );
     }
 
-    public function byFixtureIdAndMarketId(int|string $fixtureId, int|string $marketId): array
+    public function byFixtureIdAndMarketId(int|string $fixtureId, int|string $marketId): \stdClass
     {
         return $this->get(
             url: "odds/inplay/fixtures/{$fixtureId}/markets/{$marketId}"
         );
     }
 
-    public function lastUpdated(): array
+    public function lastUpdated(): \stdClass
     {
         return $this->get(
             url: 'odds/inplay/latest'
